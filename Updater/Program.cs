0df56d09a7fcdc4e842418d28d6c35483628ae26
Rod Hall <rod.hall@comparethemarket.com>
2016-07-20 14:55:50 +0100

    Moving source over to github

---
+++ Updater/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+using Newtonsoft.Json.Converters;
+using Newtonsoft.Json;
+using System.Configuration;
+using System.IO;
+
+namespace NfnProductions.BingDesktopUpdater.Updater {
+    public class Program {
+        private static void Main() {
+            if (ShouldUpdate()) {
+                Update();
+            }
+        }
+
+        private static void Update() {
+            using (var client = new WebClient()) {
+                try {
+                    client.Headers[HttpRequestHeader.UserAgent] = "Mozilla/5.0 (Windows; U; Windows NT 6.1; en-US; rv:1.9.2.13) Gecko/20101203 Firefox/3.6.13";
+                    var content = client.DownloadString(ConfigurationManager.AppSettings["BingRestUrl"].ToString());
+
+                    XDocument xDoc = XDocument.Parse(content);
+                    string fullImageUrl = xDoc.Descendants("fullImageUrl").FirstOrDefault().Value;
+
+                    if (!string.IsNullOrEmpty(fullImageUrl)) {
+                        Wallpaper.Set(new Uri(fullImageUrl), Wallpaper.Style.Stretched);
+                        ConfigurationManager.AppSettings["LastUpdated"] = DateTime.Today.ToString();
+                    }
+                }
+                catch (WebException exception) {
+                    string responseText;
+
+                    if (exception.Response != null) {
+                        var responseStream = exception.Response.GetResponseStream();
+
+                        if (responseStream != null) {
+                            using (var reader = new StreamReader(responseStream)) {
+                                responseText = reader.ReadToEnd();
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        private static bool ShouldUpdate() {
+            DateTime lastUpdated = DateTime.Parse(ConfigurationManager.AppSettings["LastUpdated"].ToString());
+            return (lastUpdated < DateTime.Today);
+        }
+    }
+}
---
+++ Updater/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Bing Desktop Updater")]
+[assembly: AssemblyDescription("Retrieve todays image from the Bing RESTful service and updates the desktop wallpaper")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("NFN Productions")]
+[assembly: AssemblyProduct("Bing Desktop Updater")]
+[assembly: AssemblyCopyright("Copyright © NFN Productions 2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("fec14e45-c66d-454b-ae1e-e143105739ed")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
